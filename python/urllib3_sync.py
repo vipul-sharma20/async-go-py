276a8caa7aeda1fb7a7782efeb90ab35fab8a5c4
Vipul <vipul.sharma20@gmail.com>
2017-08-28 00:55:01 +0530

    add sync urllib3

---
+++ python/urllib3_sync.py
@@ -0,0 +1,10 @@
+import urllib3
+
+
+def main(http):
+    return [http.request('GET', 'http://example.com') for _ in range(1000)]
+
+
+if __name__ == '__main__':
+    http = urllib3.PoolManager()
+    _ = main(http)
